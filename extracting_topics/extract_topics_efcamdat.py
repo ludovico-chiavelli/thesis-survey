91e730eceaf48cf66306435baaf738420814fd4f
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2025-02-24 14:32:59 +0000

    extract b1 and above topics

--- extracting_topics/extract_topics_efcamdat.py
+++ extracting_topics/extract_topics_efcamdat.py
@@ -5,8 +5,11 @@ def main():
     file_path = Path("Final database (main prompts).xlsx").resolve()
 
     efcd = pd.read_excel(file_path, sheet_name="Sheet 1")
-    topics = extract_topics_efcamdat(efcd)
+    topics = extract_topics_efcamdat(efcd, "efcamdat_topics.txt")
     write_topics_to_file(topics)
+
+    b1_and_above_topics = extract_b1_and_above_topics(efcd)
+    write_topics_to_file(b1_and_above_topics, "b1_and_above_topics.txt")
 
 
 def extract_topics_efcamdat(efcd):
@@ -14,9 +17,14 @@ def extract_topics_efcamdat(efcd):
     topics = efcd["topic"].unique()
     return topics
 
-def write_topics_to_file(topics):
+def extract_b1_and_above_topics(efcd):
+    # Extract the B1 and above topics from the EFCAMDAT database
+    topics = efcd[efcd["cefr_numeric"] >= 3]["topic"].unique()
+    return topics
+
+def write_topics_to_file(topics, filename):
     # Write the topics to a file
-    with open("efcamdat_topics.txt", "w") as f:
+    with open(filename, "w") as f:
         for topic in topics:
             f.write(topic + "\n")
 
