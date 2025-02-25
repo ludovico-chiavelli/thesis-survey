2f9b20209fce5f560686896f9ae9174a5aca508d
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2025-02-25 13:12:54 +0000

    add esasy topic func

--- extracting_topics/extract_topics_bawe.py
+++ extracting_topics/extract_topics_bawe.py
@@ -5,18 +5,25 @@ def main():
     file_path = Path("BAWE.xls").resolve()
 
     bawe = pd.read_excel(file_path, sheet_name="Sheet1")
-    topics = extract_topics_efcamdat(bawe)
-    write_topics_to_file(topics)
+    topics = extract_topics_bawe(bawe)
+    essay_topics = extract_topics_essay_only_bawe(bawe)
+    write_topics_to_file(topics, "bawe_topics.txt")
+    write_topics_to_file(essay_topics, "bawe_essay_topics.txt")
 
 
-def extract_topics_efcamdat(bawe):
+def extract_topics_bawe(bawe):
     # Extract the topics from the BAWE database
     topics = bawe["title"].unique()
     return topics
 
-def write_topics_to_file(topics):
+def extract_topics_essay_only_bawe(bawe):
+    # Extract the essay only topics from the BAWE database
+    topics = bawe[bawe["genre family"] == "essay"]["title"].unique()
+    return topics
+
+def write_topics_to_file(topics, filename):
     # Write the topics to a file
-    with open("bawe_topics.txt", "w") as f:
+    with open(filename, "w") as f:
         for topic in topics:
             f.write(topic + "\n")
 
