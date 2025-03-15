dccadac3fbd7b23b663247994baa9d1c940cdb39
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2025-03-15 16:41:47 +0000

    filter using LDNOOBW

--- final-fullcorpus/fc_filter_human_texts.py
+++ final-fullcorpus/fc_filter_human_texts.py
@@ -2,10 +2,10 @@ from pathlib import Path
 import pandas as pd
 import tqdm
 
-unfilterd_corpus = Path('/home/nuvolari/GitHub/thesis-survey/final-fullcorpus/final_corpus_unfiltered_texts.csv').resolve()
+unfilterd_corpus = Path('/home/nuvolari/GitHub/thesis-llm-corpus/combine-partials/final_corpus.csv').resolve()
 corpus_df = pd.read_csv(unfilterd_corpus)
 
-wordlist_filepath = Path('/home/nuvolari/GitHub/thesis-survey/final-fullcorpus/cmu-bad-words.txt').resolve()
+wordlist_filepath = Path('/home/nuvolari/GitHub/thesis-survey/final-fullcorpus/LDNOOBW-en.txt').resolve()
 with open(wordlist_filepath, 'r') as file:
     # Strip newline characters from the words.
     wordlist = [line.strip() for line in file.readlines()]
@@ -14,7 +14,7 @@ with open(wordlist_filepath, 'r') as file:
 indeces_to_drop = []
 for index, row in tqdm.tqdm(corpus_df.iterrows(), total=corpus_df.shape[0]):
     for word in wordlist:
-        if word in row['HUMAN_TEXT'].lower().split():
+        if word in row['HUMAN_TEXT'].lower().split() or word in row['MODEL_TEXT'].lower().split():
             print(f"Found bad word {word} in text at index {index}. Dropping row.")
             indeces_to_drop.append(index)
             break
